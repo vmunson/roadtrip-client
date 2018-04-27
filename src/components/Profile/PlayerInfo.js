f677f5ff2560f6dec6a41bb7950646dbaf9c9820
vmunson <virginia.munson@gmail.com>
2018-04-27 13:54:04 -0400

    update player info

--- src/components/Profile/PlayerInfo.js
+++ src/components/Profile/PlayerInfo.js
@@ -35,7 +35,7 @@ class PlayerInfo extends React.Component {
           modal: !this.state.modal,
         });
       }
-    componentWillMount() {
+    componentDidMount() {
         this.fetchPlayerInfo()
     }
     fetchPlayerInfo() {
@@ -107,10 +107,107 @@ class PlayerInfo extends React.Component {
             })
         }).then((res) => this.updatePIArray())
     }
+    view = () => {
+        if(this.state.playerInfo === [] || this.state.playerInfo === null){
+            return(
+                <div className='player'>
+                    <h1>Enter Player Information</h1>
+                    <Form id="playerinfo" onSubmit={this.handleSubmit}>
+                        <FormGroup>
+                            <Label>Player Name</Label>
+                            <Input className="form-control" type="text" name="player" placeholder="Your name" onChange={this.handleChange} />
+                            <Label>Occupation</Label>
+                            <Input className="form-control" type="select" name="occupation" onChange={this.handleChange}>
+                                <option></option>
+                                <option>Lawyer</option>
+                                <option>Teacher</option>
+                                <option>Mechanic</option>
+                                <option>Coder</option>
+                            </Input>
+                            <Label>Car</Label>
+                            <Input className="form-control" type="select" name="carType" onChange={this.handleChange}>
+                                <option></option>
+                                <option>Suburban</option>
+                                <option>Grand Caravan</option>
+                                <option>Volvo</option>
+                                <option>Mazda</option>
+                            </Input>
+                            <Button id='playerButton' type="submit" color="success">Save</Button>
+                        </FormGroup>
+                    </Form>
+                </div>
+            )
+        }else{
+            return(
+                <div className='playerUpdate'>
+                    <h3>Player Information</h3>
+                    <hr />
+                    <Table borderless>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Occupation</th>
+                                <th>Car Type</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.playerInfo.map((playerInfo, id) => {
+                                    return (
+                                        <tr key={id}>
+                                            <td scope="row">{playerInfo.player}</td>
+                                            <td>{playerInfo.occupation}</td>
+                                            <td>{playerInfo.carType}</td>
+                                            <td><Button id={playerInfo.id} size="sm" onClick={e=>this.toggle(e, playerInfo.id)} color="primary">Update</Button></td>
+                                            <td><Button id={playerInfo.id} size="sm" onClick={this.playerInfoDelete} color="danger">Delete</Button></td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </Table>
+                    <Modal isOpen={this.state.modal} className={this.props.className}>
+                        <ModalHeader>Update Player Information</ModalHeader>
+                        <Form onSubmit={this.handleUpdate}>
+                            <ModalBody>
+                                <FormGroup>
+                                <Label>Player Name</Label>
+                            <Input className="form-control" type="text" name="player" placeholder="Your name" onChange={this.handleChange} />
+                            <Label>Occupation</Label>
+                            <Input className="form-control" type="select" name="occupation" onChange={this.handleChange}>
+                                <option></option>
+                                <option>Lawyer</option>
+                                <option>Teacher</option>
+                                <option>Mechanic</option>
+                                <option>Coder</option>
+                            </Input>
+                            <Label>Car</Label>
+                            <Input className="form-control" type="select" name="carType" onChange={this.handleChange}>
+                                <option></option>
+                                <option>Suburban</option>
+                                <option>Grand Caravan</option>
+                                <option>Volvo</option>
+                                <option>Mazda</option>
+                            </Input>
+                                </FormGroup>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="success" type="submit" onClick={this.closetoggle}>Save</Button>
+                                <Button onClick={this.closetoggle}>Cancel</Button>
+                            </ModalFooter>
+                        </Form>
+                    </Modal>
+                </div>
+            )
+        }
+    }
     render() {
         return (
             <div>
-                <div className='player'>
+                {this.view()}
+                {/* <div className='player'>
                     <h1>Enter Player Information</h1>
                     <Form id="playerinfo" onSubmit={this.handleSubmit}>
                         <FormGroup>
@@ -196,7 +293,7 @@ class PlayerInfo extends React.Component {
                             </ModalFooter>
                         </Form>
                     </Modal>
-                </div>
+                </div> */}
             </div>
 
         )
